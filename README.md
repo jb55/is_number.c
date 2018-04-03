b3830b2f2a0422260fe7a3b5830e66c464bf9d34
William Casarin <jb55@jb55.com>
2018-04-03 11:28:30 -0700

    options

--- README.md
+++ README.md
@@ -29,3 +29,14 @@ int main() {
   assert(!is_num);
 }
 ```
+
+## Options
+
+```c
+enum is_num_options {
+  ALLOW_EMPTY_PRE_DOT  = 1 << 1,  // allow empty digits before the decimal point
+  ALLOW_EMPTY_POST_DOT = 1 << 2,  // allow empty digits past the decimal point
+  STRICT_WHITESPACE    = 1 << 3,  // don't allow space padding
+  ONLY_TEST_INT        = 1 << 4   // don't allow decimals/floats
+};
+```
--- is_number.h
+++ is_number.h
@@ -13,10 +13,10 @@ enum is_num_state {
 };
 
 enum is_num_options {
-  ALLOW_EMPTY_PRE_DOT = 1 << 1,
-  ALLOW_EMPTY_POST_DOT = 1 << 2,
-  STRICT_WHITESPACE = 1 << 3,
-  ONLY_TEST_INT = 1 << 4
+  ALLOW_EMPTY_PRE_DOT  = 1 << 1,  // allow empty digits before the decimal point
+  ALLOW_EMPTY_POST_DOT = 1 << 2,  // allow empty digits past the decimal point
+  STRICT_WHITESPACE    = 1 << 3,  // don't allow space padding
+  ONLY_TEST_INT        = 1 << 4   // don't allow decimals/floats
 };
 
 int is_number_with(const char *num, int len, int options);
